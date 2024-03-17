2b2251c1f3e46ea16c92c20e9ce3f664aabeac56
dimad2602 <dimad26002@gmail.com>
2024-03-17 17:41:14 +0700

    pr29

---
+++ task-stage-2-main/29/name.js
@@ -0,0 +1,25 @@
+const firstName = "Dima";
+const lastName = "Dzheglo";
+const patronymic = "Sergeevich";
+const birthYear = 2002;
+const major = "ISIT";
+
+const result1 =
+  "firstName: " +
+  firstName +
+  "\nlastName: " +
+  lastName +
+  "\npatronymic: " +
+  patronymic +
+  "\nbirthYear: " +
+  birthYear +
+  "\nmajor: " +
+  major;
+
+const result2 = `
+firstName: ${firstName} 
+lastName: ${lastName} 
+patronymic: ${patronymic} \nbirthYear: ${birthYear} \nmajor: ${major}`;
+
+console.log(result1);
+console.log(result2);
